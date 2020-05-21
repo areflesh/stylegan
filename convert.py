8fa950e8d45b0fc6c1cc89f2eb270b1be98231d6
Tim <a.reflesh@gmail.com>
2020-05-21 12:36:31 +0200

    convert

--- convert.py
+++ convert.py
@@ -1,9 +1,9 @@
 from PIL import Image
 import os 
-dir="latents_"
+dir="./raw_images"
 for i in os.listdir(dir):
     im = Image.open(dir+"/"+i)
     if im.format!="JPEG":
         rgb_im = im.convert('RGB')
-        rgb_im.save(os.path.splitext(i)[0]+'.jpg')
+        rgb_im.save(dir+'/'+os.path.splitext(i)[0]+'.jpg')
         os.remove(dir+"/"+i)
